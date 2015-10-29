d6eda580427a1c9b711d5edc525b350a5c23d27b
S00S <S0S@S0SdeMBP.lan>
2015-10-30 01:15:31 +0800

    submit

--- src/basic/Fibonacci.java
+++ src/basic/Fibonacci.java
@@ -1,16 +1,35 @@
 package basic;
 
+import java.util.Scanner;
+
 /**
-  * 测试斐波那契数列
-  */
+ * 测试斐波那契数列
+ */
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Fibonacci f = new Fibonacci();
+		System.out.print(f.fibonacci2(n) + " ");
 	}
 
-	public int fibonacci(int n) {
-		return 0;
+	public int fibonacci1(int n) {
+		if (n <= 0)
+			return 0;
+		if (n == 1)
+			return 1;
+		int pre = 0, next = 1;
+		int curr = 0;
+		for (int i = 2; i <= n; i++) {
+			curr = pre + next;
+			pre = next;
+			next = curr;
+		}
+		return curr;
 	}
 
+	public int fibonacci2(int n) {
+		return n == 1 ? 1 : fibonacci2(n - 1) + fibonacci2(n - 2);
+	}
 }
