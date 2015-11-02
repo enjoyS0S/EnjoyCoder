3f7833863fa995b85aa224f66c41b21287faa034
S00S <enjoy.S00S@gmail.com>
2015-11-02 16:33:41 +0800

    update

---
+++ src/basic/Find.java
@@ -0,0 +1,46 @@
+package basic;
+
+/**
+ * 这里主要介绍常用的查找算法
+ * 
+ * @author S0S
+ *
+ */
+public class Find {
+
+	public static void main(String[] args) {
+		int[] source = { 1, 2, 3, 4, 6, 7, 8, 12, 34, 56, 212 };
+		Find f = new Find();
+		System.out.println(f.orderFind(source, 50));
+		System.out.println(f.middleFind(source, 4));
+	}
+
+	/*
+	 * 顺序查找，顺序查找是最常见的一种查找算法，但是其效率是最低的。 它的时间复杂度为O(n)
+	 */
+	public int orderFind(int[] source, int key) {
+		for (int i = 0; i < source.length; i++) {
+			if (source[i] == key)
+				return i;
+		}
+		return -1;
+	}
+
+	/*
+	 * 二分查找，二分查找使我们用的最多的一种查找算法，效率也很高。它的时间复杂度为O(logN)
+	 */
+	public int middleFind(int[] source, int key) {
+		int start = 0, end = source.length - 1;
+		while (start <= end) {
+			int middle = (start + end) / 2;
+			if (key < source[middle])
+				end = middle - 1;
+			else if (key > source[middle])
+				start = middle + 1;
+			else
+				return middle;
+		}
+		return -1;
+	}
+
+}
