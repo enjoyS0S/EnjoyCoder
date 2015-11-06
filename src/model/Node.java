d9f81b0c75affe8ea0642b93de98fc1c8ffecdcb
S00S <enjoy.S00S@gmail.com>
2015-11-06 23:01:02 +0800

    udate

---
+++ src/model/Node.java
@@ -0,0 +1,53 @@
+package model;
+
+/**
+ * ¶þ²æÊ÷
+ * 
+ * @author S0S
+ *
+ */
+public class Node {
+
+	private Node left;
+	private Node right;
+	private int value;
+
+	public Node(int value) {
+		super();
+		this.value = value;
+		this.right = null;
+		this.left = null;
+	}
+
+	public Node(Node left, Node right, int value) {
+		super();
+		this.left = left;
+		this.right = right;
+		this.value = value;
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
