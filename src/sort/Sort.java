7da6fb0c8a1e6a104c008955c369c62a44f1e7db
S00S <enjoy.S00S@gmail.com>
2015-11-09 22:01:25 +0800

    update

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -1,13 +1,20 @@
 package sort;
 
+
 public class Sort{
 
 	public static void main(String[] args) {
 	}
-
+	
+	/*
+	 * 之间插入排序
+	 */
 	public void zhjInsert(int[] source, int n) {
 	}
 
+	/*
+	 *快速排序
+	 */
 	public void ksSort(int[] source,int n) {
 	}
 
