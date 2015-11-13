7a0c96ca55afce4ab34a6467d24c9e14b510668a
S00S <enjoy.S00S@gmail.com>
2015-11-13 11:32:58 +0800

    update

---
+++ src/sort/AgeSort.java
@@ -0,0 +1,21 @@
+package sort;
+
+/**
+ * 常用的排序有冒泡，快排，堆排，插入等。但是其实还有一些我们没有听说过，但是用处也很普遍的排序算法，例如希尔排序、桶排序
+ * 
+ * @author S0S
+ *
+ */
+public class AgeSort {
+
+	public static void main(String[] args) {
+
+	}
+
+	/*
+	 * 
+	 */
+	public int[] sortAge(int[] source, int len) {
+		return null;
+	}
+}
