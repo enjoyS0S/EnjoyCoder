a2cc86bf1b270484217fd0da490a5f8a8b7506ec
SOS <enjoy.S00S@gmail.com>
2015-11-16 21:58:24 +0800

    update

--- src/sort/AgeSort.java
+++ src/sort/AgeSort.java
@@ -16,6 +16,13 @@ public class AgeSort {
 	 * 
 	 */
 	public int[] sortAge(int[] source, int len) {
+		int[] temp = new int[99];
+		for(int i = 0; i < len; i++){
+			temp[source] ++;
+		}
+		for(int i = 0; i < len; i++){
+			source[i] = temp[0];	
+		}
 		return null;
 	}
 }
