6de7ea5ee5bd539d611e3f09c051f319a795e7b2
SOS <enjoy.S00S@gmail.com>
2015-11-22 12:08:47 +0800

    update

--- src/com/enjoy/test/list/SqListTest.java
+++ src/com/enjoy/test/list/SqListTest.java
@@ -8,16 +8,25 @@ import com.enjoy.list.SqList;
 
 public class SqListTest {
 
-	private SqList creatSqList() {
+	/**
+	 * 创建一个长度为len的线性表
+	 * @param len	线性表的长度
+	 * @return	返回线性表
+	 */
+	private SqList creatSqList(int len) {
 		Random r = new Random();
 		SqList sqList = new SqList();
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i <= len; i++) {
 			int obj = r.nextInt(20);
 			sqList.insertElemetAt(obj, i);
 		}
 		return sqList;
 	}
 
+	/**
+	 * 打印线性表
+	 * @param sqList	线性表
+	 */
 	private void printSqList(SqList sqList) {
 		for (int i = 0; i < sqList.size(); i++) {
 			System.out.print(sqList.get(i) + " ");
@@ -27,7 +36,7 @@ public class SqListTest {
 
 	@Test
 	public void testRemove() {
-		SqList sqList = creatSqList();
+		SqList sqList = creatSqList(10);
 		System.out.println("size:" + sqList.size());
 		printSqList(sqList);
 		sqList.remove(5);
