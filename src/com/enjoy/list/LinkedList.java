94a99ab1812c8f11747d1538d222c624dd1cbe78
SOS <enjoy.S00S@gmail.com>
2015-11-23 22:00:43 +0800

    update

--- src/com/enjoy/list/LinkedList.java
+++ src/com/enjoy/list/LinkedList.java
@@ -11,14 +11,14 @@ import com.enjoy.model.ListNode;
  */
 public class LinkedList<T> implements ListIntf<T> {
 
-	private ListNode<T> h = new ListNode<>(); // 头结点
+	private ListNode<T> headNode = new ListNode<>(); // 头结点
 
 	public LinkedList() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public void setH(ListNode<T> h) {
-		this.h = h;
+		this.headNode = h;
 
 	}
 
@@ -46,7 +46,7 @@ public class LinkedList<T> implements ListIntf<T> {
 	 * @param element
 	 */
 	public boolean insertElementAt(int i, T element) {
-		ListNode<T> p = h, s;
+		ListNode<T> p = headNode, s;
 		int j = 0;
 		while (p != null && j < i - 1) { // 寻找第i - 1号结点
 			p = p.next;
@@ -57,24 +57,33 @@ public class LinkedList<T> implements ListIntf<T> {
 			s.data = element;
 			s.next = p.next; // 改变指针状态
 			p.next = s;
+			System.out.println(get(0));
 			return true; // 表示插入成功
 		} else
 			return false; // 表示插入失败
 	}
 
 	/**
-	 * 查找值为x的结点
+	 * 查找值为element的结点
+	 * 
+	 * 算法思路：
+	 * 1.从第一个结点开始扫描整个单链表，将结点数据域的值逐个与element相比较
+	 * 2.找到该结点后返回指向该结点的指针
 	 * 
 	 * @param element
 	 * @return
 	 */
 	public ListNode<T> search(T element) {
-		return null;
+		ListNode<T> p = headNode.next;
+		while (p != null && p.data != element) {
+			p = p.next;
+		}
+		return p;
 	}
 
 	@Override
 	public int size() {
-		ListNode<T> p = h.next; // p指向第一个结点
+		ListNode<T> p = headNode.next; // p指向第一个结点
 		int count = 0;
 		while (p != null) { // 循环访问单链表的每个结点，p == null 时循环结束
 			count++;
@@ -86,13 +95,11 @@ public class LinkedList<T> implements ListIntf<T> {
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return size() == 0 ? true : false;
 	}
 
 	@Override
@@ -104,8 +111,8 @@ public class LinkedList<T> implements ListIntf<T> {
 	 * 4.重复2、3知道p为空或p指向第i个元素
 	 */
 	public T get(int i) {
-		int j = 0;
-		ListNode<T> p = h.next;
+		int j = 1;
+		ListNode<T> p = headNode.next;
 		while (p != null && j < i) { // 移动指针p，直到p为空或p指向第i个元素
 			p = p.next;
 			j++;
@@ -142,8 +149,24 @@ public class LinkedList<T> implements ListIntf<T> {
 
 	@Override
 	public boolean remove(T element) {
-		// TODO Auto-generated method stub
 		return false;
+	}
+
+	/**
+	 * 算法思路： 1.将q指向p结点的直接后继 2.改变指针链接，把q结点的直接后继作为p结点的直接后继 3.从单链表中删除q结点 4.释放q结点空间
+	 * 
+	 * 算法的时间复杂度为：O(1)
+	 */
+	public boolean remove(ListNode<T> p) {
+		ListNode<T> q;
+		if (p.next != null) {
+			q = p.next;
+			p.next = q.next;
+			q = null;
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
--- src/com/enjoy/test/list/LinkedListTest.java
+++ src/com/enjoy/test/list/LinkedListTest.java
@@ -1,12 +1,8 @@
 package com.enjoy.test.list;
 
-import java.util.ArrayList;
-import java.util.Stack;
-
 import org.junit.Test;
 
 import com.enjoy.list.LinkedList;
-import com.enjoy.model.ListNode;
 import com.enjoy.utils.RandomUtils;
 
 /**
@@ -17,6 +13,16 @@ import com.enjoy.utils.RandomUtils;
  */
 public class LinkedListTest {
 
+	public LinkedList<Integer> creatLinkedList(int len) {
+		LinkedList<Integer> linkedList = new LinkedList<>();
+		for (int i = 0; i < 10; i++) {
+			int randomInt = RandomUtils.getRandomInt(10);
+			System.out.println(linkedList.insertElementAt(i, randomInt) + ";"
+					+ randomInt);
+		}
+		return linkedList;
+	}
+
 	@Test
 	public void testLinkedListSize() {
 		LinkedList<Character> list = new LinkedList<>();
@@ -25,37 +31,11 @@ public class LinkedListTest {
 
 	@Test
 	public void testInsertElementAt() {
-		LinkedList<Integer> list = new LinkedList<>();
-		for (int i = 0; i <= 10; i++) {
-			int randomInt = RandomUtils.getRandomInt(10);
-			list.insertElementAt(i, randomInt);
-			System.out.print(randomInt + " ");
-		}
-		System.out.println();
-		for (int i = 0; i < list.size(); i++) {
-			System.out.print(list.get(i) + " ");
+		LinkedList<Integer> linkedList = creatLinkedList(10);
+		System.out.println("\n" + linkedList.size());
+		for (int i = 0; i < linkedList.size(); i++) {
+			System.out.print(linkedList.get(i) + " ");
 		}
 	}
 
-	@Test
-	public void test() {
-		ListNode<Integer> lnode = new ListNode<>();
-		printListFromTailToHead(lnode);
-	}
-
-	public ArrayList<Integer> printListFromTailToHead(ListNode<Integer> listNode) {
-		Stack<Integer> stack = new Stack<>();
-		while (listNode != null) {
-			stack.push(listNode.data);
-			listNode = listNode.next;
-		}
-			System.out.print("[");
-		Integer pop = stack.pop();
-		while (pop != null) {
-			System.out.print(stack.pop());
-			pop = stack.pop();
-		}
-		System.out.print("]");
-		return null;
-	}
 }
