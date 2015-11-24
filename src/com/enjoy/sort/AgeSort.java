9f59c2325f95fdeac104061aed6ce4e59bb12ea1
SOS <enjoy.S00S@gmail.com>
2015-11-24 22:30:23 +0800

    update

--- src/com/enjoy/sort/AgeSort.java
+++ src/com/enjoy/sort/AgeSort.java
@@ -8,14 +8,6 @@ package com.enjoy.sort;
  */
 public class AgeSort {
 
-	public static void main(String[] args) {
-		int[] source = { 12 };
-	}
-
-	/*
-	 * 
-	 */
-
 	public int[] sortAge(int[] source, int len) {
 		int[] temp = new int[99];
 		for (int i = 0; i < len; i++) {
